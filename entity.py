672824529ff613bb430aba37914ce1e488470d97
buoi <ptbinh2003@gmail.com>
2023-03-30 18:44:48 +0700

    add entity-manager and helper files

--- entity.py
+++ entity.py
@@ -1,21 +1,42 @@
-import re
-from abstract import Entity, EntityManager
-
-class Admin(Entity):
-	def	__init__(self, i, name, pw):
-		super().__init__(i, name)
-		self.__pw = pw
+class Entity:
+	'''Prototype for things which have a single id'''
+	def __init__(self, i, name):
+		self._id = i
+		self._name = name
+	
+	def __str__(self) -> str:
+		return f"{self._id:15}{self._name:10}"
 	
 	@property
-	def pw(self):
-		return self.__pw
+	def id(self):
+		return self._id
 	
+	@property
+	def name(self):
+		return self._name
+	
+	@name.setter
+	def name(self, name):
+		self._name = name
+
 class User(Entity):
-	def __init__(self, i, name, gender = None, email = None, year = None):
+	def __init__(self, i, name, gender, year, phone):
 		super().__init__(i, name)
 		self.__gender = gender
-		self.__email = email
 		self.__year = year
+		self.__phone = phone
+	
+	def __str__(self) -> str:
+		extra_info = f"{self.gender:3}{self.year:6}{self.phone:15}"
+		return super().__str__() + extra_info
+
+	def __eq__(self, other: 'User') -> bool:
+		match_name = self.name == other.name or not other.name
+		match_gender = self.gender == other.gender or not other.gender
+		match_year = self.year == other.year or not other.year
+		match_phone = self.phone == other.phone or not other.phone
+
+		return match_name and match_gender and match_year and match_phone
 
 	@property
 	def gender(self):
@@ -26,17 +47,12 @@ class User(Entity):
 		self.__gender = gender
 
 	@property
-	def email(self):
-		return self.__email
+	def phone(self):
+		return self.__phone
 	
-	@email.setter
-	def email(self, email):
-		regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-		if not re.match(regex, email):
-			print('Invalid email!')
-
-		else:
-			self.__email = email
+	@phone.setter
+	def phone(self, phone):	
+		self.__phone = phone
 		
 	@property
 	def year(self):
@@ -44,14 +60,13 @@ class User(Entity):
 	
 	@year.setter
 	def year(self, year):
-		if year < 0 or year > 2024 : print('Invalid year')
-		else: self.__year = year
+		self.__year = year
 
 class Category(Entity):
 	pass
 
 class Singer(Entity):
-	def __init__(self, id, name, gender = None):
+	def __init__(self, id, name, gender):
 		super().__init__(id, name)
 		self.__gender = gender
 
@@ -63,128 +78,5 @@ class Singer(Entity):
 	def gender(self, gender):
 		self.__gender = gender
 
-
-class AdminManager(EntityManager):
-	def __init__(self):
-		super().__init__()
-		self._mng_type = 'admin'
-
-	def add(self):
-		info = self.get_info()
-		if info is None: return
-
-		admin_id, admin_name = info
-		admin_pw = input('- Password: ')
-		new_admin = Admin(admin_id, admin_name, admin_pw)
-		self._data[admin_id] = new_admin
-
-	def login(self):
-		result = self.find(input('Enter ID: '))
-		pw = input('Enter password: ')
-		if result:
-			if result.pw == pw: print('Login successfully')
-			else: print('Wrong password')
-		else:
-			print('The ID does not exist')
-
-	def logout(self):
-		pass
-
-class UserManager(EntityManager):
-	def __init__(self):
-		super().__init__()
-		self._mng_type = 'user'
-
-	def add(self):
-		info = self.get_info()
-		if info is None: return
-
-		user_id, user_name = info
-		new_user = User(user_id, user_name)
-		self._data[user_id] = new_user
-
-	def update(self):
-		user = self.find(input('Enter the user ID you want to update: '))
-		if user:
-			while True:
-				req = input('You want to update: ')
-				if req == 'gender':
-					user.gender = input('Enter gender: ')
-				elif req == 'email':
-					user.email = input('Enter email: ')
-				elif req == 'year':
-					user.year = int(input('Enter year: '))
-				else: break
-		else: print('The user does not exist')
-
-	def delete(self):
-		id = input('Enter the user ID you want to delete: ')
-		user = self.find(id)
-		if user:
-			del self._data[id]
-		else: 
-			print('The user does not exist')
-
-class CategoryManager(EntityManager):
-	def __init__(self):
-		super().__init__()
-		self._mng_type = 'category'
-
-	def add(self):
-		info = self.get_info()
-		if info is None: return
-
-		category_id, category_name = info
-		category = Category(category_id, category_name)
-		self._data[category_id] = category
-
-	def delete(self):
-		id = input('Enter the category ID you want to delete: ')
-		category = self.find(id)
-		if category:
-			del self._data[id]
-		else: 
-			print('The category does not exist')
-
-class SingerManager(EntityManager):
-	def __init__(self):
-		super().__init__()
-		self._mng_type = 'singer'
-	def add(self):
-		info = self.get_info()
-		if info is None: return
-
-		singer_id, singer_name = info
-		singer = Singer(singer_id, singer_name)
-		self._data[singer_id] = singer
-
-	def update(self):
-		singer = self.find(input('Enter the singer ID you want to update: '))
-		if singer:
-			singer.gender = input('Enter the gender')
-		else: 
-			print('The singer does not exist')
-
-	def delete(self):
-		id = input('Enter the singer ID you want to delete: ')
-		singer = self.find(id)
-		if singer:
-			del self._data[id]
-		else: 
-			print('The singer does not exist')
-
-
 if __name__ == '__main__':
-	# admin = AdminManager()
-	# admin.add()
-	# admin.add()
-	# admin.login()
-
-	user = UserManager()
-	user.add()
-	user.add()
-	user.show()
-	user.update()
-	user.show()
-	user.delete()
-	user.show()
+	pass
